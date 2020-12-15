6bb343d385fdb70db5a6d97f3f26e3d81fda5b3e
aMytho <58316242+aMytho@users.noreply.github.com>
2020-12-15 15:31:21 -0600

    Updated to match the tutorial

--- Topics/API/QueryAPI/NodeJS/APIRequests.js
+++ Topics/API/QueryAPI/NodeJS/APIRequests.js
@@ -3,7 +3,7 @@
 //Questions? Talk to us in discord!
 
 var request = require('request'); //Import the library
-//Define the data we want from Glimesh
+//Define the data we want from Glimesh. 
 var data = `
 query {
 myself {
@@ -11,6 +11,17 @@ username,
 id}
 }
 `
+// If you used a client ID the myself object will not exist. Try this:
+/*
+var data = `
+query {
+user (username: "USERNAME"){
+username,
+id}
+}`
+*/
+//Make sure to replace USERNAME with a user!
+
 
 //Tell the request library where to send the request and how to add auth
 var options = {
@@ -18,7 +29,7 @@ var options = {
     body: data,
     url: 'https://glimesh.tv/api',
     headers: {
-        'Authorization': 'Bearer Token_Here'
+        'Authorization': 'Bearer Token_Here' //If you are using a client ID use 'Authorization': 'Client-ID qwertyuiop123456789' 
     }
 };
 
@@ -28,6 +39,8 @@ function callback(error, response, body) {
         console.log(body); //The unfiltered response
         var convertedResponse = JSON.parse(body); //Convert the response
         console.log(`You are ${convertedResponse.data.myself.username} and your ID is ${convertedResponse.data.myself.id}`);
+        // If you are using a client ID you will have requested the user object. Use the line below instead!
+        // console.log(`The user ${convertedResponse.data.user.username} has an ID of ${convertedResponse.data.user.id}`);
     } else {
         console.log(error); //log any errors
         console.log(body);
